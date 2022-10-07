5e4ed155f0ef6caf0010f605fa79c3e6f5097395
Gaspard <gaspard.ruan@gmail.com>
2022-10-07 13:17:55 +0800

    add:blogsView

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,38 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    sampleBlogCards: [
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-1",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-2",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-3",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-4",
+        blogDate: "May 1, 2021",
+      },
+    ],
+    editPost: null,
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+      console.log(state.editPost);
+    },
+  },
   actions: {},
   modules: {},
 });
